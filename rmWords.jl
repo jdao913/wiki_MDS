1b137bb66ba5a4a7c5c7a427bdea1333871e187c
Jeremy Dao <jeremydao913@gmail.com>
2017-12-01 16:21:13 -0800

    Added word distribution, mds CSV files for vital articles

--- rmWords.jl
+++ rmWords.jl
@@ -13,6 +13,10 @@ function main()
             help = "Output file name. Should be a csv. Saves to mdsFiles dir."
             arg_type = String
             default = "out.jld"
+        "--common", "-c"
+            help = "Input file name of common words to take out. If not given, no words are taken out."
+            arg_type = String
+            default = nothing
     end
 
     parsed_args = parse_args(ARGS, s)
@@ -20,6 +24,8 @@ function main()
     filename = parsed_args["filename"]
     outfile = parsed_args["outfile"]
     outfile = string("./miner/parsedArticles/", outfile)
+    common = parsed_args["common"]
+    
 
     file = CSV.read(filename, nullable=false);
     println("Size of file: ", size(file))
@@ -27,10 +33,24 @@ function main()
     numArt = size(artNames)[1]
     artWords = Array(file[2:end])      #List of words in articles
     data = Array(file[:, 2:end]);
-    numWords = 30000
+    numWords = 20000
+
+    if common != nothing
+        commonfile = open(common)
+        words = readlines(commonfile)
+        commonWords = split(words[1], ",")
+        takeOut = intersect(artWords, commonWords)
+        for i = 1:size(commonWords)[1]
+            if any(x->x==artWords[i], takeOut)
+                data[:,i] = 0           #Set all common word occurances to 0
+            end
+        end
+    end
+
     totalWords = sum(data, 1)
     println("data: ", size(data))
     println("totalWords: ", size(totalWords), typeof(totalWords))
+
     newData = Array{Int64, 2}(numArt, numWords)
     println("newData", size(newData))
     for i = 1:numWords
